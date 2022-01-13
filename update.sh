cb2b87f3610c4e1ba54e733de3ad94c040dccb0e
kohmet <k.j.hirner.wisdom@gmail.com>
2022-01-13 01:30:41 -0800

    updated HolzCraftsAdmin.py.bak for compile

---
+++ update.sh
@@ -0,0 +1,11 @@
+#! /usr/bin/env bash
+
+./encryptor.py s2.py
+cp licensed.py ~/.local/bin/holzcraftsadmin.py
+sudo chattr -i ~/key.txt
+cp key.txt ~/key.txt
+sudo chattr +i ~/key.txt
+
+sudo chattr -i ~/settings.cfg
+cp settings.cfg ~/settings.cfg
+sudo chattr +i ~/settings.cfg
