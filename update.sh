12bc4a69468dcd04289a83082d1c1eccb4bede69
kohmet <k.j.hirner.wisdom@gmail.com>
2022-01-27 12:24:37 -0800

    added new tool options

--- update.sh
+++ update.sh
@@ -9,7 +9,7 @@ sudo chattr +i ~/key.txt
 sudo chattr -i ~/settings.cfg
 cp settings.cfg ~/settings.cfg
 sudo chattr +i ~/settings.cfg
+
 git add . 
 git commit -am "added new tool options"
-git push https://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@github.com/0rion-HunterShield/Home2Bar_Qt-DRM-Tools
-
+git push
