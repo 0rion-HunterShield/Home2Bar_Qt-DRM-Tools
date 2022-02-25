5a5e76a183483e225b8d97fbc5880c61dba93b13
kohmet <k.j.hirner.wisdom@gmail.com>
2022-02-25 12:41:26 -0800

    added new tool options

--- update.sh
+++ update.sh
@@ -2,16 +2,17 @@
 
 ./encryptor.py s2.py
 cp licensed.py ~/.local/bin/holzcraftsadmin.py
+
 sudo chattr -i ~/key.txt
 cp key.txt ~/key.txt
-sudo chattr +i ~/key.txt
 
-sudo chattr -i ~/settings.cfg
-cp settings.cfg ~/settings.cfg
-sudo chattr +i ~/settings.cfg
+
+sudo chattr -i ~/.settings.cfg
+cp settings.cfg ~/.settings.cfg
+
 
 echo "username: 0rion-HunterShield"
 echo "Token: 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"
-git add . 
+git add .
 git commit -am "added new tool options"
 git push
