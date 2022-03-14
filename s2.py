76ed49635c4ed5f86be8111d5f9994ac785ca80e
kohmet <k.j.hirner.wisdom@gmail.com>
2022-03-14 04:22:42 -0700

    added new tool options

--- s2.py
+++ s2.py
@@ -3906,7 +3906,7 @@ align:center;
         self.window.ocr_imageView.fitInView(QRectF(0,0,30,30),Qt.AspectRatioMode.KeepAspectRatio)
         self.ocr_scene.addItem(self.ocr_pix)
     update_address='https://github.com/0rion-HunterShield/MDI180-QT6'
-    code_version='HCA17'
+    code_version='HCA17.1'
     def update_info(self):
         response=requests.get('{server}/holzcraftsframes/updates/'.format(server=self.window.server.text()),headers={'Authorization':'Token {}'.format(self.window.token.text())},verify=False)
         print(response)
